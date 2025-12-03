1a1ab50afedd1de75faa835f16c0fd8e5ae02d3e
Atto9752 <anto.faundes@duocuc.cl>
2025-12-03 16:16:31 -0300

    avance en controller

--- usuarios/src/main/java/com/example/stroms/usuarios/controller/usuarioControlador.java
+++ usuarios/src/main/java/com/example/stroms/usuarios/controller/usuarioControlador.java
@@ -3,6 +3,10 @@ package com.example.stroms.usuarios.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,22 +20,28 @@ public class usuarioControlador {
     @Autowired //para que cuando se levante el controlador se inicie el servicio 
     private usuarioService servicio;
 
+
+    @GetMapping
     public List<usuario> listarUsuarios() {
         return servicio.listarUsuarios();
     }
 
+    @PostMapping
     public usuario guardarUsuario(usuario usuario) {
         return servicio.guardarUsuario(usuario);
     }
 
+    @DeleteMapping("/{id}")
     public void eliminarUsuario(int id) {
         servicio.eliminarUsuario(id);
     }
 
+    @GetMapping("/{id}")
     public usuario buscarUsuario(int id) {
         return servicio.buscarUsuario(id);
     }
 
+    @PutMapping("/{id}")    
     public usuario actualizarUsuario(int id, usuario usuarioActualizado) {
         return servicio.actualizarUsuario(id, usuarioActualizado);
     }
