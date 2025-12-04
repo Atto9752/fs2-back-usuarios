33f0b07fe61e771ca4ca8dfdc692894e0d4067fb
Atto9752 <anto.faundes@duocuc.cl>
2025-12-04 12:41:10 -0300

    correcciones
    añade el repositorio de roles, se corrige el service para poder inyectar y acceder a los roles, se corrige el metodo de guardarUsuario en base a esto

--- usuarios/src/main/java/com/example/stroms/usuarios/service/usuarioService.java
+++ usuarios/src/main/java/com/example/stroms/usuarios/service/usuarioService.java
@@ -1,12 +1,15 @@
 package com.example.stroms.usuarios.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.stroms.usuarios.model.dto.rol;
 import com.example.stroms.usuarios.model.dto.usuario;
 import com.example.stroms.usuarios.repository.usuarioRepositorio;
+import com.example.stroms.usuarios.repository.rolRepositorio;
 
 @Service
 public class usuarioService {
@@ -14,12 +17,25 @@ public class usuarioService {
     @Autowired //para que se inicie junto con el repo y no se desconecten uno del otro
     private usuarioRepositorio usuarioRepo;
 
+    @Autowired // inyeccion apra buscar roles
+    private rolRepositorio rolRepo;
+
     public List<usuario> listarUsuarios() {
         return usuarioRepo.findAll();
     }
 
     public usuario guardarUsuario(usuario usuario) {
+
+        Optional<rol> rolCliente = rolRepo.findByNombreRol("CLIENTE");
+
+        if (rolCliente.isEmpty()) {
+            throw new RuntimeException("El rol CLIENTE no esta configurado en la base de datos.");
+        }
+
+        usuario.setRol(rolCliente.get());
+
         return usuarioRepo.save(usuario);
+
     }
 
     public void eliminarUsuario(int id) {
